a133a1d9794dac8804a9aecfb5f0771467b19b54
PRAWINM <36333404+PRAWINM@users.noreply.github.com>
2018-06-26 21:19:13 +0530

    Update countno of digits.py

--- countno of digits.py
+++ countno of digits.py
@@ -1,6 +1,6 @@
-Number = int(input("Please Enter any Number: "))
+Number = int(input())
 Count = 0
 while(Number > 0):
     Number = Number // 10
     Count = Count + 1
-print("\n Number of Digits in a Given Number = %d" %Count)
+print(Count)
