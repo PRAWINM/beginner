9378cfa4a2fad3d3096710565ba991c72a02d976
PRAWINM <36333404+PRAWINM@users.noreply.github.com>
2018-07-09 22:19:01 +0530

    Update nelemprinmin.py

--- nelemprinmin.py
+++ nelemprinmin.py
@@ -1,5 +1,4 @@
-num = int(input("enter the number of elements:"))
-a=[]
-for i in range(0,num):
-  a.append(int(input()))
-print(min(a))
+b=int(input())
+a=list(map(int,input().split(' ')))[:b]
+a.sort()
+print(a[0])
