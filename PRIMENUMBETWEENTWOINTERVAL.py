ca39c5b98117032e7208615a9593ce6a1fa235b4
PRAWINM <36333404+PRAWINM@users.noreply.github.com>
2018-07-09 21:34:40 +0530

    Update PRIMENUMBETWEENTWOINTERVAL.py

--- PRIMENUMBETWEENTWOINTERVAL.py
+++ PRIMENUMBETWEENTWOINTERVAL.py
@@ -1,9 +1,8 @@
-a = int(input("Enter lower range: "))
-b = int(input("Enter upper range: "))
+a,b=map(int,input().split(" "))
 for num in range(a,b + 1):
-   if num > 1:
+   if(num>1):
        for i in range(2,num):
-           if (num % i) == 0:
+           if (num%i == 0):
                break
        else:
            print(num)
