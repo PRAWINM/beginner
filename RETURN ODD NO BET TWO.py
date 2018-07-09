1b0d91c98252cdb1b3381cd36819dd35836e8b44
PRAWINM <36333404+PRAWINM@users.noreply.github.com>
2018-07-09 21:27:42 +0530

    Update RETURN ODD NO BET TWO.py

--- RETURN ODD NO BET TWO.py
+++ RETURN ODD NO BET TWO.py
@@ -1,5 +1,4 @@
-a=int(input("Enter the number1:"))
-b=int(input("Enter the number2:"))
-for i in range(a+1,b+1):
+a,b=map(int,input().split(" "))
+for i in range(a+1,b):
   if(i%2!=0):
     print(i)
